8d1ac157a50cbd1c3ee99c6b14b2fdaf6bacb206
Clampe3 <clampe3@huskers.unl.edu>
2023-04-03 15:19:23 -0500

    Reading and Writing given path

--- directory_search.py
+++ directory_search.py
@@ -1,5 +1,20 @@
 import glob
+import os
+import sys
+
 def get_movie_list(root):
     movies = glob.glob('**/*.txt', root_dir = root, recursive=True)
     return movies
 
+def write_file(path, text): # relative
+    file = os.path.basename(path)
+    os.chdir(path.removesuffix(os.path.basename(path)))
+    with open (file, "wt") as f:
+        f.write(text)
+
+def read_file(path):
+    file = os.path.basename(path)
+    os.chdir(path.removesuffix(os.path.basename(path)))
+    with open (os.path.basename(path), "r") as f:
+        text = f.read()
+        return text
