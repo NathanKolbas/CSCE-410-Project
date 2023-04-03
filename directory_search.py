67991267292b8874b9ba1d4cf705f3d8c8e0b535
Nathan Kolbas <nathan2000@outlook.com>
2023-04-03 14:13:06 -0500

    Restructered the files

--- directory_search.py
+++ directory_search.py
@@ -1,5 +1,6 @@
 import glob
-def get_movie_list(root):
-    movies = glob.glob('**/*.txt', root_dir = root, recursive=True)
-    return movies
 
+
+def get_movie_list(root):
+    movies = glob.glob('**/*.txt', root_dir=root, recursive=True)
+    return movies
---
+++ inverted_index.py
@@ -0,0 +1,121 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+
+class Appearance:
+    """
+    Represents the appearance of a term in a given document, along with the
+    frequency of appearances in the same one.
+    """
+
+    def __init__(self, doc_id, frequency):
+        self.doc_id = doc_id
+        self.frequency = frequency
+
+    def __repr__(self):
+        """
+        String representation of the Appearance object
+        """
+        return str(self.__dict__)
+
+
+class Database:
+    """
+    In memory database representing the already indexed documents.
+    """
+
+    def __init__(self):
+        self.db = dict()
+
+    def __repr__(self):
+        """
+        String representation of the Database object
+        """
+        return str(self.__dict__)
+
+    def get(self, id):
+        return self.db.get(id, None)
+
+    def add(self, document):
+        """
+        Adds a document to the DB.
+        """
+        return self.db.update({document['id']: document})
+
+    def remove(self, document):
+        """
+        Removes document from DB.
+        """
+        return self.db.pop(document['id'], None)
+
+
+def from_doc_to_terms(doc):
+    """
+    Takes a doc (giant string) and gets the terms
+    """
+    tokens = nltk.word_tokenize(doc.strip())
+    tokens = [word.lower() for word in tokens if word.isalnum() and word.lower() not in stopwords.words('english')]
+    return tokens
+
+
+class InvertedIndex:
+    """
+    Inverted Index class.
+    """
+
+    def __init__(self, db, preprocess=False):
+        self.index = dict()
+        self.db = db
+        self.preprocess = preprocess
+
+    def __repr__(self):
+        """
+        String representation of the Database object
+        """
+        return str(self.index)
+
+    def index_document(self, document):
+        """
+        Process a given document, save it to the DB and update the index.
+        """
+        if self.preprocess:
+            terms = from_doc_to_terms(document['text'])
+            appearances_dict = dict()
+            for term in terms:
+                appearances_dict[term] = Appearance(document['id'], 0)
+        else:
+            # Remove punctuation from the text.
+            clean_text = re.sub(r'[^\w\s]', '', document['text'])
+            terms = clean_text.split(' ')
+            appearances_dict = dict()
+            # Dictionary with each term and the frequency it appears in the text.
+            for term in terms:
+                term_frequency = appearances_dict[term].frequency if term in appearances_dict else 0
+                appearances_dict[term] = Appearance(document['id'], term_frequency + 1)
+
+        # Update the inverted index
+        update_dict = {key: [appearance]
+        if key not in self.index
+        else self.index[key] + [appearance]
+                       for (key, appearance) in appearances_dict.items()}
+        self.index.update(update_dict)
+        # Add the document into the database
+        self.db.add(document)
+        return document
+
+    def lookup_query(self, query):
+        """
+        Returns the dictionary of terms with their correspondent Appearances.
+        This is a very naive search since it will just split the terms and show
+        the documents where they appear.
+        """
+        if self.preprocess:
+            return {term: self.index[term] for term in from_doc_to_terms(query) if term in self.index}
+        else:
+            return {term: self.index[term] for term in query.split(' ') if term in self.index}
