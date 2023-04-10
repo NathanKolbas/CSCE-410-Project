522c7d7aee0c8af4f182aa94a482884f7bca386a
Nathan Kolbas <nathan2000@outlook.com>
2023-04-10 15:06:31 -0500

    Added commandline args for using the tool

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CSCE-410-Project
+
+## Algorithm Notes 
+```python
+print(index.db) => {'db': {'./test.txt': {'id': './test.txt', 'text': 'Bee Movie Script - ... Special thanks to SergeiK.'}}}
+print(index.index) => {'Bee': [{'doc_id': './test.txt', 'frequency': 16}], 'Movie': [{'doc_id': './test.txt', 'frequency': 4}], ... }
+```  
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import argparse
+import os
+
+from directory_search import get_movie_list
 from inverted_index import Database, InvertedIndex
+
+
+# Setup commandline args
+parser = argparse.ArgumentParser(
+    prog='TODO Change Later: Some kind of special index',
+    description='Build an index for all files in a directory',
+    epilog=' - A Team of Softies',
+)
+
+# The starting directory, defaults to CWD. Can specify a relative path to the CWD.
+parser.add_argument('-d', '--dir', default=os.getcwd(), type=str)
+# If the index should be built
+parser.add_argument('-c', '--compile')
+
+# The commandline args
+args = parser.parse_args()
 
 
 def highlight_term(id, term, text):
@@ -20,24 +40,56 @@ def load_documents():
     return docs
 
 
+def load_document(f_path):
+    with open(f_path, 'r', encoding="utf8", errors='ignore') as textfile:
+        data = textfile.read().replace('\n', ' ').strip()
+        return {
+            'id': f_path,
+            'text': data
+        }
+
+
 if __name__ == '__main__':
-    print('Hello World!')
-    db = Database()
-    index = InvertedIndex(db, False)
-    documents = load_documents()
-    for document in documents:
+    print('------ STARTING ------')
+    is_absolute_path = os.path.isabs(args.dir)
+
+    # The root path to start at
+    root_path = args.dir if is_absolute_path else os.path.join(os.getcwd(), args.dir)
+
+    # The relative path from the root
+    movie_paths = get_movie_list(root_path)
+    # The full path to the movies
+    movie_paths_full = [os.path.join(root_path, p) for p in movie_paths]
+
+    print(movie_paths)
+    print(movie_paths_full)
+
+    for path in movie_paths:
+        db = Database()
+        index = InvertedIndex(db, False)
+        document = load_document(path)
         index.index_document(document)
+        print(index.db)
+        print(index.index)
 
-    print(index.db)
-    print(index.index)
 
-    while True:
-        search_term = input("Enter term(s) to search: ")
-        result = index.lookup_query(search_term)
-
-        for term in result.keys():
-            for appearance in result[term]:
-                # Belgium: { doc_id: 1, frequency: 1}
-                document = db.get(appearance.doc_id)
-                print(highlight_term(appearance.doc_id, term, document['text']))
-            print("-----------------------------")
+    # print('Hello World!')
+    # db = Database()
+    # index = InvertedIndex(db, False)
+    # documents = load_documents()
+    # for document in documents:
+    #     index.index_document(document)
+    #
+    # print(index.db)
+    # print(index.index)
+    #
+    # while True:
+    #     search_term = input("Enter term(s) to search: ")
+    #     result = index.lookup_query(search_term)
+    #
+    #     for term in result.keys():
+    #         for appearance in result[term]:
+    #             # Belgium: { doc_id: 1, frequency: 1}
+    #             document = db.get(appearance.doc_id)
+    #             print(highlight_term(appearance.doc_id, term, document['text']))
+    #         print("-----------------------------")
