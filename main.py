718890773562b8ec790df128f8d6b2eee7250a5a
Nathan Kolbas <nathan2000@outlook.com>
2023-04-16 23:41:34 -0500

    Added searching the indexes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
 import os
+import inverted_index
 
 from directory_search import get_index_files
 from lict_data import LictConfig
-from models.tree import Tree
+from models.tree import Tree, Node
 
 # Setup commandline args
 parser = argparse.ArgumentParser(
@@ -20,14 +21,16 @@ parser.add_argument('-i', '--index', default=False, type=bool)
 parser.add_argument('-e', '--extension', default='.txt', type=str)
 # Removes all built index files
 parser.add_argument('-c', '--clean', default=False, type=bool)
+# Begin search
+parser.add_argument('-s', '--search', default=True, type=bool)
 
 # The commandline args
 args = parser.parse_args()
 
 
-def highlight_term(id, term, text):
+def highlight_term(doc_id, term, text):
     replaced_text = text.replace(term, "\033[1;32;40m {term} \033[0;0m".format(term=term))
-    return "--- document {id}: {replaced}".format(id=id, replaced=replaced_text)
+    return "--- document {id}: {replaced}".format(id=doc_id, replaced=replaced_text)
 
 
 def main():
@@ -66,26 +69,52 @@ def main():
         tree.build_index(root_path, config, args.extension)
         return
 
-    # print('Hello World!')
-    # db = Database()
-    # index = InvertedIndex(db, False)
-    # documents = load_documents()
-    # for document in documents:
-    #     index.index_document(document)
-    #
-    # print(index.db)
-    # print(index.index)
-    #
-    # while True:
-    #     search_term = input("Enter term(s) to search: ")
-    #     result = index.lookup_query(search_term)
-    #
-    #     for term in result.keys():
-    #         for appearance in result[term]:
-    #             # Belgium: { doc_id: 1, frequency: 1}
-    #             document = db.get(appearance.doc_id)
-    #             print(highlight_term(appearance.doc_id, term, document['text']))
-    #         print("-----------------------------")
+    if args.search:
+        print('Booting up search...')
+
+        search_sub_dir = ''
+        show_file_content = False
+        while True:
+            print(f'Searching directory (to change use chdir=DIR): ./{search_sub_dir}')
+            search_term = input("Enter term(s) to search: ")
+
+            if 'exit' == search_term:
+                print('Exiting...')
+                break
+
+            if 'chdir=' in search_term:
+                search_term = search_term.replace('chdir=', '')
+                search_sub_dir = search_term
+                print(f'Changed search directory to: ./{search_sub_dir}')
+                continue
+
+            if 'show-file-content=' in search_term:
+                search_term = search_term.replace('show-file-content=', '')
+                show_file_content = search_term.lower() == 'true'
+                if show_file_content:
+                    print('Enabled show file content')
+                else:
+                    print('Disabled show file content')
+                continue
+
+            index_path = os.path.join(root_path, search_sub_dir, Node.combined_indexes_full_filename)
+            index = inverted_index.InvertedIndex.open(index_path)
+            if index is None:
+                print('Index file not found...')
+                continue
+
+            result = index.lookup_query(search_term)
+            for term in result.keys():
+                for appearance in result[term]:
+                    doc_id = appearance['doc_id']
+                    if show_file_content:
+                        file = os.path.join(root_path, appearance['doc_id'])
+                        with open(file, 'r', encoding="utf8", errors='ignore') as textfile:
+                            document = textfile.read()
+                            print(highlight_term(doc_id, term, document))
+                    else:
+                        print(f'--- document {doc_id}')
+                print("-----------------------------")
 
 
 if __name__ == '__main__':
